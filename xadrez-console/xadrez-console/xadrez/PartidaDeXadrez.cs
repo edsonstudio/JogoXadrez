946293b9a4a2dd582245b2065abf42a3828546d1
Edson <edsont8@hotmail.com>
2020-05-20 13:22:16 -0300

    Jogada especial Promocao

--- xadrez-console/xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO.Compression;
+using System.Security.Cryptography;
 using tabuleiro;
 
 namespace xadrez
@@ -142,6 +143,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em XEQUE!!!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial Promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -159,7 +175,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-            Peca p = tab.peca(destino);
 
             // #jogadaespecial En Passant
             if (p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha +2))
